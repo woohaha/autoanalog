b1f24ee4d496834cd2df7d8224c662cb335609f1
woohaha <realwoohaha@gmail.com>
2012-11-26 11:50:34 +0800

    不提示，直接删除

--- clear_rar_and_dir.sh
+++ clear_rar_and_dir.sh
@@ -2,8 +2,8 @@
 echo -e "Press Enter to execute,abort by pressing <CTRL-C>"
 read 
 rm iislog-*.rar
-rm -r 190/ 229/
-rm -r 231/ 269/
-rm -r 0092/
-rm -r 0119/ 0378/ 0185/
+rm -rf 190/ 229/
+rm -rf 231/ 269/
+rm -rf 0092/
+rm -rf 0119/ 0378/ 0185/
 echo "Done!"
