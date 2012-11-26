6e75ae48fc18cfa85019849e08f73d602e05f66e
nearby <nearby@nearby.(none)>
2012-11-26 11:43:04 +0800

    加入清理脚本

---
+++ clear_rar_and_dir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -e "Press Enter to execute,abort by pressing <CTRL-C>"
+read 
+rm iislog-*.rar
+rm -r 190/ 229/
+rm -r 231/ 269/
+rm -r 0092/
+rm -r 0119/ 0378/ 0185/
+echo "Done!"
